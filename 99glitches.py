42419fe4f4ff0e9b4d71a21ca398d539304132d8
eff <foo@bar.com>
2011-09-08 23:39:23 -0700

    pre cleanup

---
+++ 99glitches.py
@@ -0,0 +1,84 @@
+## 99glitches -- a codex toy
+
+import Tkinter as tk
+import os, random, copy, OSC, StringIO, time
+from PIL import Image, ImageTk
+
+class nnGlitches:
+
+	index = 0
+	images = []
+	master = []
+
+	def __init__(self,path,root):
+		
+		self.path = path
+		self.root = root
+		
+		self.panel = tk.Label(root)
+		self.image_list = os.listdir(path)
+
+		# build the images array
+		for image in self.image_list:
+			print image
+			f = open(path + "/" + image,"rb")
+			self.images.append(bytearray(f.read()))
+			self.master = copy.deepcopy(self.images)	
+	
+	def glitch(self):
+		
+		for _ in range(random.randint(1,3)):
+			
+			byte_array = self.images[self.index]		
+			rand = random.randint(500,len(byte_array)-1)
+			byte_array[rand] = random.randint(0,255)			
+			
+			try:
+				image1 = ImageTk.PhotoImage(Image.open(StringIO.StringIO(byte_array)))
+			
+				panel = tk.Label(root, image=image1)
+				panel.pack()
+			 
+				# save the panel's image from 'garbage collection'
+				print 'draw' + str(self.index)
+				panel.image = image1
+				self.panel.destroy()
+				self.panel = panel
+				
+				# image glitch is successful, rewrite to the array for reglitchin'
+				self.images[self.index] = byte_array
+			except:
+				# the image is fucked. refresh it. 
+				self.images[self.index] = self.master[self.index]
+				print "the image is fucked"
+				pass
+		
+		if(random.randint(0,1)):
+			if self.index >= len(self.image_list)-1: self.index=0
+			else: self.index+=1
+			root.after(100, self.glitch)
+		else: 
+			root.after(20, self.glitch)
+
+		# set callback
+
+## init
+root = tk.Tk()
+root.title('99glitches')
+root.geometry("%dx%d+%d+%d" % (800,600, 0, 0))
+
+glitch_obj = nnGlitches('img',root)
+
+# start sound
+client = OSC.OSCClient()
+client.connect( ('127.0.0.1', 57120) )
+msg = OSC.OSCMessage()
+msg.setAddress("/cmd")
+msg.append("start")
+client.send(msg)
+
+# give audio time to spool -- hack!
+time.sleep(3)
+
+root.after(100, glitch_obj.glitch)
+root.mainloop()
