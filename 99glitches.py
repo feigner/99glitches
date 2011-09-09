4bc4e32b4a9c85ce0bfd8a96f69c06c14ad6d05e
eric feigner <eric@weareinstrument.com>
2011-09-09 08:48:06 -0700

    tweakin' things, adding sample images, README.

--- 99glitches.py
+++ 99glitches.py
@@ -1,5 +1,6 @@
 ## 99glitches -- a codex toy
 
+
 import Tkinter as tk
 import os, random, copy, OSC, StringIO, time
 from PIL import Image, ImageTk
@@ -20,13 +21,13 @@ class nnGlitches:
 
 		# build the images array
 		for image in self.image_list:
-			print image
 			f = open(path + "/" + image,"rb")
 			self.images.append(bytearray(f.read()))
 			self.master = copy.deepcopy(self.images)	
 	
 	def glitch(self):
 		
+		# randomly apply 1 to 3 levels of glitch
 		for _ in range(random.randint(1,3)):
 			
 			byte_array = self.images[self.index]		
@@ -35,32 +36,30 @@ class nnGlitches:
 			
 			try:
 				image1 = ImageTk.PhotoImage(Image.open(StringIO.StringIO(byte_array)))
-			
+				
 				panel = tk.Label(root, image=image1)
 				panel.pack()
-			 
-				# save the panel's image from 'garbage collection'
-				print 'draw' + str(self.index)
 				panel.image = image1
+				
 				self.panel.destroy()
 				self.panel = panel
 				
 				# image glitch is successful, rewrite to the array for reglitchin'
 				self.images[self.index] = byte_array
 			except:
-				# the image is fucked. refresh it. 
+				# the image is effd. refresh it. 
 				self.images[self.index] = self.master[self.index]
-				print "the image is fucked"
+				print "readd @ " + str(self.index)
 				pass
 		
+		# 50/50 chance of reprocessing the same image
 		if(random.randint(0,1)):
 			if self.index >= len(self.image_list)-1: self.index=0
 			else: self.index+=1
 			root.after(100, self.glitch)
+			print self.index
 		else: 
 			root.after(20, self.glitch)
-
-		# set callback
 
 ## init
 root = tk.Tk()
@@ -68,17 +67,6 @@ root.title('99glitches')
 root.geometry("%dx%d+%d+%d" % (800,600, 0, 0))
 
 glitch_obj = nnGlitches('img',root)
-
-# start sound
-client = OSC.OSCClient()
-client.connect( ('127.0.0.1', 57120) )
-msg = OSC.OSCMessage()
-msg.setAddress("/cmd")
-msg.append("start")
-client.send(msg)
-
-# give audio time to spool -- hack!
-time.sleep(3)
 
 root.after(100, glitch_obj.glitch)
 root.mainloop()
